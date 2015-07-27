d74d70ea167222d53466ccd592d500d9319d37dd
Ellie Lee <luckycharms2496@gmail.com>
2015-07-26 22:22:03 -0700

    Create index_php.php
    create index_php.php

---
+++ index_php.php
@@ -0,0 +1,58 @@
+try{
+
+	if(!empty($_POST) && isset($_SERVER['HTTP_X_REQUESTED_WITH'])){
+
+		// Output a JSON header
+
+		header('Content-type: application/json');
+
+		// Is the email address valid?
+
+		if(!isset($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+			throw new Exception('Please enter a valid email.');
+		}
+
+		// This will throw an exception if the person is above 
+		// the allowed login attempt limits (see functions.php for more):
+		rate_limit($_SERVER['REMOTE_ADDR']);
+
+		// Record this login attempt
+		rate_limit_tick($_SERVER['REMOTE_ADDR'], $_POST['email']);
+
+		// Send the message to the user
+
+		$message = '';
+		$email = $_POST['email'];
+		$subject = 'Your Login Link';
+
+		if(!User::exists($email)){
+			$subject = "Thank You For Registering!";
+			$message = "Thank you for registering at our site!\n\n";
+		}
+
+		// Attempt to login or register the person
+		$user = User::loginOrRegister($_POST['email']);
+
+		$message.= "You can login from this URL:\n";
+		$message.= get_page_url()."?tkn=".$user->generateToken()."\n\n";
+
+		$message.= "The link is going expire automatically after 10 minutes.";
+
+		$result = send_email($fromEmail, $_POST['email'], $subject, $message);
+
+		if(!$result){
+			throw new Exception("There was an error sending your email. Please try again.");
+		}
+
+		die(json_encode(array(
+			'message' => 'Thank you! We\'ve sent a link to your inbox. Check your spam folder as well.'
+		)));
+	}
+}
+catch(Exception $e){
+
+	die(json_encode(array(
+		'error'=>1,
+		'message' => $e->getMessage()
+	)));
+}
