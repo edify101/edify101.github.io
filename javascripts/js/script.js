dffe0dd586fa377f0ba175e15b2694efab4b2446
Elizabeth Siegle <lizzie.siegle@gmail.com>
2015-08-19 11:47:13 -0700

    ...

--- javascripts/js/script.js
+++ javascripts/js/script.js
@@ -2,47 +2,48 @@
 
 $(function(){
 
-    // Checking for CSS 3D transformation support
-    $.support.css3d = supportsCSS3D();
+    console.log("yash");
 
-    var formContainer = $('#formContainer');
+    // // Checking for CSS 3D transformation support
+    // $.support.css3d = supportsCSS3D();
 
-    // Listening for clicks on the ribbon links
-    $('.flipLink').click(function(e){
+    // var formContainer = $('#formContainer');
 
-        // Flipping the forms
-        formContainer.toggleClass('flipped');
+    // // Listening for clicks on the ribbon links
+    // $('.flipLink').click(function(e){
 
-        // If there is no CSS3 3D support, simply
-        // hide the login form (exposing the recover one)
-        if(!$.support.css3d){
-            $('#login').toggle();
-        }
-        e.preventDefault();
-    });
+    //     // Flipping the forms
+    //     formContainer.toggleClass('flipped');
 
-    formContainer.find('form').submit(function(e){
-        // Preventing form submissions. If you implement
-        // a backend, you will want to remove this code
-        e.preventDefault();
-    });
+    //     // If there is no CSS3 3D support, simply
+    //     // hide the login form (exposing the recover one)
+    //     if(!$.support.css3d){
+    //         $('#login').toggle();
+    //     }
+    //     e.preventDefault();
+    // });
 
-    // A helper function that checks for the
-    // support of the 3D CSS3 transformations.
-    function supportsCSS3D() {
-        var props = [
-            'perspectiveProperty', 'WebkitPerspective', 'MozPerspective'
-        ], testDom = document.createElement('a');
+    // formContainer.find('form').submit(function(e){
+    //     // Preventing form submissions. If you implement
+    //     // a backend, you will want to remove this code
+    //     e.preventDefault();
+    // });
 
-        for(var i=0; i<props.length; i++){
-            if(props[i] in testDom.style){
-                return true;
-            }
-        }
+    // // A helper function that checks for the
+    // // support of the 3D CSS3 transformations.
+    // function supportsCSS3D() {
+    //     var props = [
+    //         'perspectiveProperty', 'WebkitPerspective', 'MozPerspective'
+    //     ], testDom = document.createElement('a');
 
-        return false;
+    //     for(var i=0; i<props.length; i++){
+    //         if(props[i] in testDom.style){
+    //             return true;
+    //         }
+    //     }
+
+    //     return false;
     }
 });
 
 
-console.log("yash");
